69c45bd56f2a079a6e4997f4e646fc1bf54af306
walterwenzel <144962219+walterwenzel@users.noreply.github.com>
2023-12-11 15:08:04 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -2,12 +2,10 @@ function validarLogin() {
     var usuario = document.getElementById("usuario").value;
     var contraseña = document.getElementById("contraseña").value;
     var mensajeError = document.getElementById("mensajeError");
-
-    // Lógica de validación simple (puedes personalizarla según tus necesidades)
+    
     if (usuario === "admin" || contraseña === "1234") {
         mensajeError.textContent = "¡Por favor, completa todos los campos!";
-    } else {
-        // Envía el formulario si la validación es exitosa
+    } else {        
         document.getElementById("loginForm").submit();
     }
 }
